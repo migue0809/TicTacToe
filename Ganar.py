af8f650186d720d7da2d94ea94ab76d41f22e106
Miguel Santiago Ramirez <miguetiago0809@gmail.com>
2018-08-16 02:44:01 -0500

    Change in Ganar

--- Ganar.py
+++ Ganar.py
@@ -1,43 +1,43 @@
-def ganar(SA):
-    Empate=posMov(SA)
+def Ganar(SA):
+    Empate=PosMov(SA)
     for i in range(3):
-        if EA[i][0] == 1 and EA[i][1] == 1 and EA[i][2] == 1:
+        if SA[i][0] == 1 and SA[i][1] == 1 and SA[i][2] == 1:
             print('You are the champion')
             n=1
             break
-        elif EA[i][0] ==2 and EA[i][1] == 2 and EA[i][2] == 2:
+        elif SA[i][0] ==2 and SA[i][1] == 2 and SA[i][2] == 2:
             print('You are not the champion')
             n=-1
             break
-        if EA[0][i] ==1 and EA[1][i] ==1 and EA[2][i] ==1:
+        if SA[0][i] == 1 and SA[1][i] == 1 and SA[2][i] == 1:
             print('You are the champion')
             n=1
             break
-        elif EA[0][i] ==2 and EA[1][i] ==2 and EA[2][i] ==2:
+        elif SA[0][i] ==2 and SA[1][i] ==2 and SA[2][i] ==2:
             print('You are not the champion')
             n=-1
             break
-        if EA[0][0] == 1 and EA[1][1] == 1 and EA[2][2] == 1:
+        if SA[0][0] == 1 and SA[1][1] == 1 and SA[2][2] == 1:
             print('You are the champion')
             n=1
             break
-        elif EA[0][0] == 2 and EA[1][1] == 2 and EA[2][2] == 2:
+        elif SA[0][0] == 2 and SA[1][1] == 2 and SA[2][2] == 2:
             print('You are not the champion')
             n=-1
             break
-        if EA[0][2] == 1 and EA[1][1] == 1 and EA[2][0] == 1:
+        if SA[0][2] == 1 and SA[1][1] == 1 and SA[2][0] == 1:
             print('You are the champion')
             n=1
             break
-        elif EA[0][2] == 2 and EA[1][1] == 2 and EA[2][0] == 2:
+        elif SA[0][2] == 2 and SA[1][1] == 2 and SA[2][0] == 2:
             print('You are not the champion')   
             n=-1
             break
-    if Empate==0:
-        print('There is not the champion') 
-        n=0
-    else:
-        print('You can play')
-        n=2
-    
+        if Empate==0:
+            print('There is not the champion') 
+            n=0
+        else:
+            print('You can play')
+            n=2
+    return n
     return n
