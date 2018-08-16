15bf09994ca8c96fe39f13459cdf81566b8ad63f
Miguel Santiago Ramirez <miguetiago0809@gmail.com>
2018-08-16 05:53:31 -0500

    Change in compRuta

--- compRuta.py
+++ compRuta.py
@@ -1,11 +1,16 @@
-def compRuta(VR,Heuristica,SA):
-    TamañoH=len(Heuristica)
-    Move=PosMov(SA)
-    Heuristica1=-1000
-    for i in range(TamañoH):
-        if Heuristica[i]>Heuristica1:
-            Heuristica1=Heuristica[i]+Heuristica1
-    CR=VR
-            
+MovexVHeu=posMov(CR)+1
+VH=np.zeros((1,MovexVHeu),dtype=int)
+VM=np.zeros((MovexVHeu,3,3),dtype=int)
+
+def compRuta(CR,Heuristica,VM,VH,T):
+    a=Ruta[1,0]
+    VM[a-1]=CR
+    VH[a-1]=Heuristica+VH[a-1]
     
-    return Heuristica1,CR
+    return VM, VH
+    
+    if (T==True)
+        MejorH=max(VH)
+        MejorPos=np.where(MejorH,VH)
+    
+        return VM[MejorPos], MejorH
